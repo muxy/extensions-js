3ab2b4ea0231706bfa38b70f5a1f9de63beb23f5
Colin Edwards <colin@recursivepenguin.com>
2017-06-19 17:39:41 -0500

    Add some jsdoc comments

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import User from './user';
 
 import * as PackageConfig from '../package.json';
 
+/**
+ * MuxyExtensionsSDK is a wrapper around the Muxy SDK that allows querying and submitting data.
+ * It will detect when it is not being run on Twitch and fetch test credentials so that you don't
+ * need to dev directly on Twitch.
+ */
 class MuxyExtensionsSDK {
+
+  /**
+   *
+   * @param {string} extensionID
+   * @param {object} options
+   */
   constructor(extensionID, options = {}) {
     const SDKInfoText = [
       'Muxy Extensions SDK',
@@ -15,6 +26,7 @@ class MuxyExtensionsSDK {
       ''
     ];
 
+    /** @private */
     this.extensionID = extensionID;
 
     if (options.testAppID) {
@@ -40,8 +52,11 @@ class MuxyExtensionsSDK {
 
     consolePrint(SDKInfoText, { boxed: true });
 
+    /** @private */
     this.loadPromise = new Promise((resolve, reject) => {
+      /** @private */
       this.loadResolve = resolve;
+      /** @private */
       this.loadReject = reject;
     });
 
@@ -54,8 +69,11 @@ class MuxyExtensionsSDK {
       if (this.client) {
         this.client.updateAuth(auth.token);
       } else {
+        /** @private */
         this.client = new Client(this.extensionID, auth.token, auth.channelId);
+        /** @private */
         this.analytics = new Analytics(auth.userId);
+        /** @private */
         this.user = new User(this.client, auth);
       }
 
@@ -75,6 +93,8 @@ class MuxyExtensionsSDK {
 
   /**
    * Fetch accumulated data
+   * @param {string} accumulationID
+   * @param start
    */
   getAccumulation(accumulationID, start) {
     return this.client.getAccumulation(accumulationID, start);
@@ -82,14 +102,27 @@ class MuxyExtensionsSDK {
 
   /**
    * Send data to be accumulated
+   * @param {string} accumulationID
+   * @param data
    */
   accumulate(accumulationID, data) {
     return this.client.accumulate(accumulationID, data);
   }
 
   /**
+   * @typedef VoteData
+   * @type {object}
+   * @property {number} stddev
+   * @property {number} mean
+   * @property {number} sum
+   * @property {number} specific
+   * @property {number} count
+   */
+
+  /**
    * Fetch the current vote data
-   * @returns an object with the structure {stddev, mean, sum, specific, count}
+   * @param {string} voteID
+   * @returns {VoteData}
    */
   getVoteData(voteID) {
     return this.client.getVotes(voteID);
@@ -97,8 +130,8 @@ class MuxyExtensionsSDK {
 
   /**
    * Submit a vote
-   * @param voteID the poll that this vote is for
-   * @param value the integer value to vote
+   * @param {string} voteID the poll that this vote is for
+   * @param {number} value the integer value to vote
    */
   vote(voteID, value) {
     return this.client.vote(voteID, {
@@ -107,8 +140,16 @@ class MuxyExtensionsSDK {
   }
 
   /**
+   * @typedef RankDatum
+   * @type {object}
+   * @property {string} key
+   * @property {number} score
+   */
+
+  /**
    * Fetch the current ranking data
-   * @returns an array of rank data objects in the form {key: "testkey", score: 23}
+   * @param {string} rankID
+   * @returns {RankDatum[]} an array of rank data objects
    */
   getRankingData(rankID) {
     return new Promise((accept, reject) => {
@@ -124,6 +165,8 @@ class MuxyExtensionsSDK {
 
   /**
    * Submit data to be ranked
+   * @param {string} rankID
+   * @param {string} value
    */
   rank(rankID, value) {
     return this.client.rank({

b766cb9b4ec7f6c8e86f3d6c098c47d06065c2fe
Colin Edwards <colin@recursivepenguin.com>
2017-06-19 18:00:01 -0500

    Refresh the test token every 25 minutes

--- src/twitch-ext.js
+++ src/twitch-ext.js
@@ -2,17 +2,22 @@ import Client from './state-client';
 
 // Wrapper around global Twitch extension object.
 class Ext {
+  static fetchTestAuth(cb) {
+    Client.fetchTestAuth(this.testAppID, this.testChannelID)
+      .then((auth) => {
+        cb(auth);
+      })
+      .catch(() => {
+        cb();
+      });
+  }
+
   static onAuthorized(cb) {
     if (window.Twitch) {
       window.Twitch.ext.onAuthorized(cb);
     } else {
-      Client.fetchTestAuth(this.testAppID, this.testChannelID)
-        .then((auth) => {
-          cb(auth);
-        })
-        .catch(() => {
-          cb();
-        });
+      Ext.fetchTestAuth(cb);
+      setInterval(Ext.fetchTestAuth, 1000 * 60 * 25, cb);
     }
   }
 

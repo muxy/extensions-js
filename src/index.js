c7ac63d0a018b1be7f3e28553b335d3b043045e3
Cameron Rivers <cameron.rivers@gmail.com>
2017-06-29 18:11:23 -0500

    Move Muxy top-level object away from ES2015 class
    Instead Muxy() is now a function that creates an underlaying API object.
    This allows us to create SDK() objects more naturally with `new Muxy.SDK(id)`

--- src/index.js
+++ src/index.js
@@ -7,99 +7,97 @@ import Messenger from './messenger';
 
 import * as PackageConfig from '../package.json';
 
-/**
- * The Muxy class is used to create entry points into the extensions SDK.
- * It is
- */
-class Muxy {
-  constructor() {
-    // Channel to use when in testing env (currently Lirik)
-    // user can change this with Muxy.testChannelID before Muxy.SDK
-    this.testChannelID = '23161357';
+function Muxy() {
+  const muxy = {};
 
-    // Role to create an auth token with in testing env
-    // user can change this with Muxy.testJWTRole before Muxy.SDK
-    this.testJWTRole = 'viewer';
+  // Channel to use when in testing env (currently Lirik)
+  // user can change this with Muxy.testChannelID before Muxy.SDK
+  muxy.testChannelID = '23161357';
 
-    this.SDKClients = {};
+  // Role to create an auth token with in testing env
+  // user can change this with Muxy.testJWTRole before Muxy.SDK
+  muxy.testJWTRole = 'viewer';
 
-    this.client = new Client();
-    this.messenger = new Messenger();
+  muxy.SDKClients = {};
 
-    this.loadPromise = new Promise((resolve, reject) => {
-      this.loadResolve = resolve;
-      this.loadReject = reject;
-    });
+  muxy.client = new Client();
+  muxy.messenger = new Messenger();
 
-    const SDKInfoText = [
-      'Muxy Extensions SDK',
-      `v${PackageConfig.version} © ${(new Date()).getFullYear()} ${PackageConfig.author}`,
-      PackageConfig.repository,
-      ''
-    ];
+  muxy.loadPromise = new Promise((resolve, reject) => {
+    muxy.loadResolve = resolve;
+    muxy.loadReject = reject;
+  });
 
-    switch (CurrentEnvironment()) {
-      case ENVIRONMENTS.DEV:
-        SDKInfoText.push('Running in development mode');
-        break;
-      case ENVIRONMENTS.STAGING:
-        SDKInfoText.push('Running in staging environment');
-        break;
-      case ENVIRONMENTS.PRODUCTION:
-        SDKInfoText.push('Running on production');
-        break;
-      case ENVIRONMENTS.SERVER:
-        SDKInfoText.push('Running on a NodeJS server');
-        break;
-      default:
-        SDKInfoText.push('Could not determine execution environment.');
-    }
+  const SDKInfoText = [
+    'Muxy Extensions SDK',
+    `v${PackageConfig.version} © ${(new Date()).getFullYear()} ${PackageConfig.author}`,
+    PackageConfig.repository,
+    ''
+  ];
 
-    consolePrint(SDKInfoText, { boxed: true });
+  switch (CurrentEnvironment()) {
+    case ENVIRONMENTS.DEV:
+      SDKInfoText.push('Running in development mode');
+      break;
+    case ENVIRONMENTS.STAGING:
+      SDKInfoText.push('Running in staging environment');
+      break;
+    case ENVIRONMENTS.PRODUCTION:
+      SDKInfoText.push('Running on production');
+      break;
+    case ENVIRONMENTS.SERVER:
+      SDKInfoText.push('Running on a NodeJS server');
+      break;
+    default:
+      SDKInfoText.push('Could not determine execution environment.');
   }
 
-  watchAuth(extensionID) {
+  consolePrint(SDKInfoText, { boxed: true });
+
+  muxy.watchAuth = (extensionID) => {
     Ext.appID = extensionID;
     Ext.testChannelID = this.testChannelID;
     Ext.testJWTRole = this.testJWTRole;
 
     Ext.onAuthorized((auth) => {
       if (!auth) {
-        this.loadReject();
+        muxy.loadReject();
       }
 
-      this.messenger.extensionID = auth.clientId;
-      this.messenger.channelID = auth.channelId;
-      this.client.updateAuth(auth.token);
-      this.user = new User(auth);
+      muxy.messenger.extensionID = auth.clientId;
+      muxy.messenger.channelID = auth.channelId;
+      muxy.client.updateAuth(auth.token);
+      muxy.user = new User(auth);
 
-      const keys = Object.keys(this.SDKClients);
+      const keys = Object.keys(muxy.SDKClients);
       for (let i = 0; i < keys.length; i += 1) {
-        this.SDKClients[keys[i]].user = this.user;
+        muxy.SDKClients[keys[i]].user = muxy.user;
       }
 
-      this.loadResolve();
+      muxy.loadResolve();
     });
 
     // Ext.onContext(this.onContext);
-  }
+  };
 
   /**
    * Returns a SDK to use
    */
-  SDK(extensionID) {
-    if (!this.SDKClients[extensionID]) {
-      this.SDKClients[extensionID] = new SDK(extensionID, this.client, this.user,
-        this.messenger, this.loadPromise);
+  muxy.SDK = function MuxySDK(extensionID) {
+    if (!muxy.SDKClients[extensionID]) {
+      muxy.SDKClients[extensionID] = new SDK(extensionID, muxy.client, muxy.user,
+        muxy.messenger, muxy.loadPromise);
     }
 
-    if (!this.watchingAuth) {
-      this.watchingAuth = true;
-      this.watchAuth(extensionID);
+    if (!muxy.watchingAuth) {
+      muxy.watchingAuth = true;
+      muxy.watchAuth(extensionID);
     }
 
-    return this.SDKClients[extensionID];
-  }
+    return muxy.SDKClients[extensionID];
+  };
+
+  return muxy;
 }
 
 // Do this so that the entrypoint as a global library is correct.

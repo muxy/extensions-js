a70098c2bc51a1022ed3ccc4261380333abe4271
Cameron Rivers <cameron.rivers@gmail.com>
2017-08-30 11:57:11 -0500

    Docs on User object

--- src/user.js
+++ src/user.js
@@ -1,54 +1,130 @@
-// User stores fields related to the current extension user
-// in vuex, and manages updating/accessing those fields.
+/**
+ * User stores fields related to the current extension user, either a viewer or the broadcaster.
+ * These fields are automatically updated by the SDK.
+ */
 export default class User {
+  /**
+   * @since 1.0.0
+   * @param {object} auth - An auth token usable by this user for backend requests.
+   */
   constructor(auth) {
-    // channelID holds the numeric id of the channel the user is currently watching.
+    /**
+     * channelID holds the numeric id of the channel the user is currently watching.
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.channelID = auth.channelId;
 
-    // twitchJWT holds the raw JWT response from the Twitch Extension SDK.
+    /**
+     * twitchJWT holds the raw JWT response from the Twitch Extension SDK.
+     *
+     * @since 1.0.0
+     * @type {object}
+     */
     this.twitchJWT = auth.token;
 
-    // twitchOpaqueID is a Twitch generated ID that will uniquely identify this
-    // user (if they are logged in), but does not give us access to their
-    // Twitch ID.
+    /**
+     * twitchOpaqueID is a Twitch generated ID that will uniquely identify this
+     * user (if they are logged in), but does not give us access to their Twitch ID.
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.twitchOpaqueID = auth.userId;
 
-    // twitchID is this viewer's actual Twitch ID. Used to coordinate access to
-    // other Twitch services and across the Twitch universe.
+    /**
+     * twitchID is this viewer's actual Twitch ID. Used to coordinate access to
+     * other Twitch services and across the Twitch universe. Only set if the user
+     * grants access, null otherwise.
+     *
+     * @since 1.0.0
+     * @type {null|string}
+     */
     this.twitchID = null;
 
-    // True if the user has an active muxy account.
+    /**
+     * registeredWithMuxy will be true if the user has an active muxy account.
+     *
+     * @since 1.0.0
+     * @type {boolean}
+     */
     this.registeredWithMuxy = false;
 
-    // muxyID is this viewer's ID on Muxy. Used to allow configuration and access
-    // to Twitch services from Muxy.
+    /**
+     * muxyID is this viewer's ID on Muxy. Null if the user has not authenticated with
+     * Muxy or is not sharing their Twitch ID with the extension.
+     *
+     * @since 1.0.0
+     * @type {null|string}
+     */
     this.muxyID = null;
 
-    // visualizationID is a unique user string that can be used to identify this user
-    // on Muxy's a.muxy.io subdomain. This is used for things like alerts and
-    // cheer visualizations.
+    /**
+     * visualizationID is a unique user string that can be used to identify this user
+     * on Muxy's a.muxy.io subdomain. This is used for things like alerts and
+     * cheer visualizations, but is not generally useful. Empty string if
+     * `registeredWithMuxy` is false.
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.visualizationID = '';
 
-    // role is the current user's role in the extension. May be one of
-    // ['viewer', 'config'].
+    /**
+     * role is the current user's role in the extension. May be one of ['viewer', 'config'].
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.role = 'viewer';
 
-    // ip is the user's ip address as returned from the `UserInfo` state endpoint.
+    /**
+     * ip is the current user's IP address. May be an empty string if undetectable.
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.ip = '';
 
-    // Current game title being played by streamer.
+    /**
+     * game is the title of the current channel's game as set by the broadcaster.
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.game = '';
 
-    // Current video mode: One of ['default', fullscreen', 'theatre'].
+    /**
+     * Current video mode: One of ['default', fullscreen', 'theatre'].
+     *
+     * @since 1.0.0
+     * @type {string}
+     */
     this.videoMode = 'default';
 
-    // Current video bitrate.
+    /**
+     * Current video bitrate. Null if no video or unknown.
+     *
+     * @since 1.0.0
+     * @type {null|number}
+     */
     this.bitrate = null;
 
-    // Current video latency.
+    /**
+     * Current video latency. Null if no video or unknown.
+     *
+     * @since 1.0.0
+     * @type {null|number}
+     */
     this.latency = null;
 
-    // Current buffer size.
+    /**
+     * Current buffer size of the viewer's player. Null if no video or uknown.
+     *
+     * @since 1.0.0
+     * @type {null|number}
+     */
     this.buffer = null;
 
     // If the user has authorized an extension to see their Twitch ID, it will be
@@ -56,8 +132,12 @@ export default class User {
     this.extractJWTInfo(auth.token);
   }
 
-  // extractJWTInfo attempts to parse the provided JWT and persist any found
-  // information in store.
+  /**
+   * Attempts to parse the provided JWT and persist any found information in store.
+   * @since 1.0.0
+   *
+   * @param {object} jwt - The auth JWT token as returned from the auth harness.
+   */
   extractJWTInfo(jwt) {
     try {
       const splitToken = jwt.split('.');
@@ -73,15 +153,26 @@ export default class User {
     }
   }
 
-  // anonymous returns whether or not the current extension user is anonymous.
-  // Twitch defines an anonymous user as one who is not logged in to the channel
-  // page running this extension, or one who has explicitly opted-out from sharing
-  // auth information with this extension.
+  /**
+   * Returns whether or not the current extension user is anonymous.
+   * Twitch defines an anonymous user as one who is not logged in to the channel
+   * page running this extension, or one who has not opted-in to sharing
+   * auth information with this extension.
+   * @since 1.0.0
+   *
+   * @return {boolean} True if the user is not logged in to Twitch or has not granted
+   * access to their Twitch ID.
+   */
   anonymous() {
     return !this.twitchOpaqueID || this.twitchOpaqueID[0] !== 'U';
   }
 
-  // updateAuth stores values from a new auth token in the local store.
+  /**
+   * Stores values from a new auth token in the local store.
+   * @since 1.0.0
+   *
+   * @param {object} auth - An auth JWT with updated user information.
+   */
   updateAuth(auth) {
     this.twitchJWT = auth.token;
     this.extractJWTInfo(auth.token);
